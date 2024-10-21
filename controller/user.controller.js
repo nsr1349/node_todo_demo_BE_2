1897da470a459a99d56d35118c276d9de92b5a22
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-22 00:25:36 +0900

    feat: 에러 메세지 수정 및 토큰 복구

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -7,16 +7,17 @@ const userController = {}
 userController.createUser = async (req, res) => {
     try {
         const { email , name, password } = req.body 
+        if (!email || !name || !password ) throw new Error('모든 칸을 입력해줘')
         const user = await User.findOne({ email })
 
-        if (user) throw new Error('이미 가입되어 있단다')
+        if (user) throw new Error('이미 가입되어 있엉')
 
         const newUser = new User({ email , name, password : bcrypt.hashSync(password, bcrypt.genSaltSync(saltRounds))})
         await newUser.save()
 
         res.status(200).json({status : 'success', data : newUser})
-    } catch (err) {
-        res.status(400).json({status : 'fail', err})
+    } catch ({message}) {
+        res.status(400).json({status : 'fail', err : message})
     }
 }
 
@@ -27,13 +28,15 @@ userController.loginWithEmail = async (req, res) => {
         if (user){
             const isMath = bcrypt.compareSync(password, user.password)
             if (isMath){
-                // const token = user.generateToken()
-                // return res.status(200).json({status : 'success', user, token})
-                return res.status(200).json({status : 'success', user})
+                const token = user.generateToken()
+                return res.status(200).json({status : 'success', user, token})
             }
+        } else {
+            throw new Error('가입을 안한 것 같은데')
         }
-    } catch (err) {
-        res.status(400).json({status : 'fail', err})
+        throw new Error('아이디나 비밀번호가 뭔가 다름')
+    } catch ({message}) {
+        res.status(400).json({status : 'fail', err : message})
     }
 }
 
