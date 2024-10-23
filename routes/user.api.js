d5b03fe0cf344525815368d29f7643d250ddba8c
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-23 23:06:50 +0900

    feat: getUser Route 정의

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { createUser, loginWithEmail } = require('../controller/user.controller')
+const { createUser, loginWithEmail , getUser } = require('../controller/user.controller')
+const { authenticate } = require('../controller/auth.controller')
 
 router.post('/', createUser)
 
 router.post('/login', loginWithEmail)
+
+router.get('/' , authenticate, getUser)
 
 module.exports = router
