b606e1a7004f3a44b8e70e3eec2982e782fb3a9e
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-23 23:47:59 +0900

    feat: 할 일 api 기능 추가

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -5,7 +5,7 @@ const taskController = {}
 taskController.createTask = async (req, res) => {
     try {
         const { task , isComplete } = req.body 
-        const newTask = new Task({ task , isComplete })
+        const newTask = new Task({ task , isComplete, author : req.userId })
         await newTask.save()
         res.status(200).json({status : 'ok', data : newTask})
     } catch (err) {
@@ -15,7 +15,9 @@ taskController.createTask = async (req, res) => {
 
 taskController.getTask = async (req, res) => {
     try {
-        const taskList = await Task.find({}).select('-__v').sort({ createdAt: -1 })
+        const { userId } = req.params
+        const findFilter = userId ? { author : userId } : {}
+        const taskList = await Task.find(findFilter).populate('author').sort({ createdAt: -1 })
         res.status(200).json({status : 'ok', data : taskList})
     } catch (err) {
         res.status(400).json({status : 'fail', err})
